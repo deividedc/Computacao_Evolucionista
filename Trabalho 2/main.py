8617655eed35b30d8891c693b9866b2c0120d093
deividedc <delarota.devid@gmail.com>
2025-07-21 12:12:53 -0300

    resultados com elitismoo de 0 a 5 individuos

--- Trabalho 2/main.py
+++ Trabalho 2/main.py
@@ -16,7 +16,7 @@ parametros = {
     'TAXA_MUTACAO':        0.02,     # Probabilidade de mutação 
     'SEMENTE_BASE':        42,       # Semente base para garantir reprodutibilidade das execuções
     'TORNEIO_K':           7,        # Número de indivíduos no torneio para seleção (maior = mais seletivo)
-    'NUM_ELITISTAS':       0,        # Quantidade de melhores indivíduos mantidos (elitismo)
+    'NUM_ELITISTAS':       4,        # Quantidade de melhores indivíduos mantidos (elitismo)
     'MAX_SEM_MELHORA':     100000,   # Número máximo de gerações sem melhoria antes de resetar população
     'SELECAO':             'torneio' # Método de seleção utilizado ('torneio' ou 'selecao_roleta')
 }
